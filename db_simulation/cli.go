80d53c4deba97168dc4e4afef7ae9059ccf6754d
Bruce Chen <aaaeeettt124@126.com>
2024-09-22 06:48:31 +0800

    fj

--- db_simulation/cli.go
+++ db_simulation/cli.go
@@ -56,3 +56,14 @@ func cli_mydb(db *gorm.DB) {
 	}
 
 }
+
+func schema_show(db *gorm.DB, table_input string) {
+	var tables []string
+	var schema1 string
+	db.Raw("SELECT name FROM sqlite_master WHERE type='table'").Scan(&tables)
+	fmt.Println("Tables in the database:", tables)
+	fmt.Println("------------------***-----------------")
+
+	db.Raw("SELECT sql FROM sqlite_master WHERE type='table' AND name = ?", table_input).Scan(&schema1)
+	fmt.Printf("Schema of table %s:\n%s\n", table_input, schema1)
+}
